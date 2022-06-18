82fd44c2542f134e6e4c8eaee7c034333dc3cbae
Jean Carlos Jimenez Dominguez <jeansoft@hotmail.com>
2022-06-18 09:35:40 -0400

    adding other navigation files

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+import { useStateContext } from "../contexts/ContextProvider";
+
+const Button = ({
+  color,
+  bgColor,
+  text,
+  borderRadius,
+  size,
+  width,
+  bgHoverColor,
+}) => {
+  return (
+    <button
+      type="button"
+      style={{ backgroundColor: bgColor, color, borderRadius }}
+      className={` text-${size} p-3 w-${width} hover:drop-shadow-xl hover:bg-${bgHoverColor}`}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button;
